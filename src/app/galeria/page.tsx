bfe88db1fb9aa2d8694ea635f26ae5188a1f35c9
CranckThatFranck <franck.oliver@outlook.com.br>
2026-02-06 13:02:08 -0300

    feat: implement image search functionality and enhance ImageCard component

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,14 +1,59 @@
-import { Template } from "@/components";
-import { ImageCard } from "@/components";
+"use client";
+import { Template, ImageCard } from "@/components";
+import { Image } from "@/resources/image/image.resource";
+import { useImageService } from "@/resources/image/image.service";
+import { useState } from 'react';
 
 export default function GaleriaPage() {
+
+  const useService = useImageService();
+  const [images, setImages] = useState<Image[]>([]);
+  const [query, setQuery] = useState<string>("");
+  const [extension, setExtension] = useState<string>("");
+
+  async function searchImages(){
+    console.log("valor digitado:", query)
+    const result = await useService.buscar(query, extension);
+    setImages(result);
+  }
+
+  function renderImageCard(image: Image){
+    return (
+      <ImageCard key={image.url}
+        nome={image.name} 
+        src={image.url} 
+        tamanho={image.size} 
+        dataUpload={image.uploadDate} />
+    )
+  }
+
+  function renderImageCards(){
+    return images.map(renderImageCard);
+  }
+
   return (
     <Template>
-        <section className="grid grid-cols-3 gap-8">
-        <ImageCard nome="Montanha" dataUpload="04/02/2026" tamanho="2mb" src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="/>
-        <ImageCard src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70"/>
-        <ImageCard />
-        </section>
+
+     <section className="flex flex-col items-center justify-center my-5">
+        <div className="flex space-x-4">
+          <input type="text"
+                 onChange={(event) => setQuery((event.target as HTMLInputElement).value)} 
+                 className="border px-3 py-2 rounded-lg text-gray-900" />
+          <select onChange={(event) => setExtension((event.target as HTMLSelectElement).value)} 
+                  className="border px-4 py-2 text-gray-900 px-3 py-2 rounded-lg">
+            <option value="">Todas as categorias</option>
+            <option value="PNG">PNG</option>
+            <option value="JPEG">JPEG</option>
+            <option value="GIF">GIF</option>
+          </select>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={searchImages}>Buscar</button>
+          <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg" onClick={searchImages}>+ Imagem</button>
+        </div>
+     </section>
+
+      <section className="grid grid-cols-4 gap-4">  
+        {renderImageCards()}
+      </section>
     </Template>
   );
 }
--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 interface ImageCardProps {
     nome?: string;
     tamanho?: string;
@@ -6,8 +8,13 @@ interface ImageCardProps {
 }
 
 export const ImageCard: React.FC<ImageCardProps> = (props: ImageCardProps) => {
+
+    function download(){
+        window.open(props.src, "_blank");
+    }
+
     return (
-        <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:translate-y-2">
+        <div onClick={download} className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:translate-y-2">
             <img src={props.src} className="h-56 w-full object-cover rounded-t-md" alt=""/>
             <div className="card-body p-4">
                 <h5 className="text-x1 font-semibold mb-2 text-gray-600">{props.nome}</h5>
